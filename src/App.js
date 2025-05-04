d738acba72075bea51e13e5847aa9508a0e12bf6
Tanvir Hassan Anik <tanvir.h.anik@gmail.com>
2025-05-04 18:09:36 +0600

    slight cleaning

--- src/App.js
+++ src/App.js
@@ -3,63 +3,47 @@ import { useEffect, useState } from 'react';
 import './App.css';
 const githubAccessToken = process.env.REACT_APP_GITHUB_PAT;
 
-
-
-// function SearchButton({setUserList,userListAll,setSearchInputValue,searchInputValue}){
-//   function handleChange(inputFieldEvent){
-//     const currentText = inputFieldEvent.target.value;
-//     setSearchInputValue(currentText);
-//     const updatedUserList = userListAll.filter((user)=>{return user.login.toLowerCase().includes(currentText.trim().toLowerCase());});
-//     setUserList(updatedUserList);
-//   }
-//   return <input type='text' placeholder='Search user name' value={searchInputValue} onChange={handleChange}/>
-// }
-
 function App() {
-
-  // const [userListAll, setUserListAll] = useState([]);
   const [userList, setUserList] = useState([]);
   const [nextPageUrl, setNextPageUrl] = useState(null);
-  // const [searchInputValue,setSearchInputValue] = useState("");
 
   useEffect(()=>{
     getUserList("https://api.github.com/users");
   },[]);
 
-    async function getUserList(url){
-      try{
-        const users = await axios.get(url,{
-          headers: {
-            Authorization: `Bearer ${githubAccessToken}`
-          }
-        });
-
-    function parseLinkHeader(header) {
-      const linkHeadersArray = header.split(', ');
-      const linkHeadersMap = {};
-    
-      linkHeadersArray.forEach(link => {
-        const [urlPart, relPart] = link.split('; ');
-        const url = urlPart.slice(1, -1); // remove < and >
-        const rel = relPart.replace('rel="', '').replace('"', '');
-        linkHeadersMap[rel] = url;
+  async function getUserList(url){
+    try{
+      const users = await axios.get(url,{
+        headers: {
+          Authorization: `Bearer ${githubAccessToken}`
+        }
       });
-    
-      return linkHeadersMap;
-    }
-        // setUserListAll(users.data);
-        setUserList(users.data);
 
-        const linkHeader = users.headers.link;
-        const linkParsed = parseLinkHeader(linkHeader);
-        console.log(linkParsed.next);
-        setNextPageUrl(linkParsed.next);
-
-
-      } catch (error) {
-        console.log(error);
+      function parseLinkHeader(header) {
+        const linkHeadersArray = header.split(', ');
+        const linkHeadersMap = {};
+      
+        linkHeadersArray.forEach(link => {
+          const [urlPart, relPart] = link.split('; ');
+          const url = urlPart.slice(1, -1); // remove < and >
+          const rel = relPart.replace('rel="', '').replace('"', '');
+          linkHeadersMap[rel] = url;
+        });
+      
+        return linkHeadersMap;
       }
+      setUserList(users.data);
+
+      const linkHeader = users.headers.link;
+      const linkParsed = parseLinkHeader(linkHeader);
+      console.log(linkParsed.next);
+      setNextPageUrl(linkParsed.next);
+
+
+    } catch (error) {
+      console.log(error);
     }
+  }
 
   function UserListTable(){
     return (
@@ -99,7 +83,6 @@ function App() {
   return (
     <div className="App">
       <main>
-        {/* <SearchButton setUserList = {setUserList}userListAll={userListAll}setSearchInputValue={setSearchInputValue} searchInputValue={searchInputValue}/> */}
         <UserListTable/>
         <Pagination/>
       </main>
