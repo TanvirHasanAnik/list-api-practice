ff949308f12ece661b414802cf987264f643d499
Tanvir Hassan Anik <tanvir.h.anik@gmail.com>
2025-05-04 18:06:58 +0600

    pagination: next page feature added

--- src/App.js
+++ src/App.js
@@ -1,42 +1,65 @@
-import './App.css';
 import axios from 'axios';
-import { useState,useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import './App.css';
 const githubAccessToken = process.env.REACT_APP_GITHUB_PAT;
 
 
 
-function SearchButton({setUserList,userListAll,setSearchInputValue,searchInputValue}){
-  function handleChange(inputFieldEvent){
-    const currentText = inputFieldEvent.target.value;
-    setSearchInputValue(currentText);
-    const updatedUserList = userListAll.filter((user)=>{return user.login.toLowerCase().includes(currentText.trim().toLowerCase());});
-    setUserList(updatedUserList);
-  }
-  return <input type='text' placeholder='Search user name' value={searchInputValue} onChange={handleChange}/>
-}
+// function SearchButton({setUserList,userListAll,setSearchInputValue,searchInputValue}){
+//   function handleChange(inputFieldEvent){
+//     const currentText = inputFieldEvent.target.value;
+//     setSearchInputValue(currentText);
+//     const updatedUserList = userListAll.filter((user)=>{return user.login.toLowerCase().includes(currentText.trim().toLowerCase());});
+//     setUserList(updatedUserList);
+//   }
+//   return <input type='text' placeholder='Search user name' value={searchInputValue} onChange={handleChange}/>
+// }
 
 function App() {
 
-  const [userListAll, setUserListAll] = useState([]);
+  // const [userListAll, setUserListAll] = useState([]);
   const [userList, setUserList] = useState([]);
-  const [searchInputValue,setSearchInputValue] = useState("");
+  const [nextPageUrl, setNextPageUrl] = useState(null);
+  // const [searchInputValue,setSearchInputValue] = useState("");
 
   useEffect(()=>{
-    async function getUserList(){
+    getUserList("https://api.github.com/users");
+  },[]);
+
+    async function getUserList(url){
       try{
-        const users = await axios.get("https://api.github.com/users",{
+        const users = await axios.get(url,{
           headers: {
             Authorization: `Bearer ${githubAccessToken}`
           }
         });
-        setUserListAll(users.data);
+
+    function parseLinkHeader(header) {
+      const linkHeadersArray = header.split(', ');
+      const linkHeadersMap = {};
+    
+      linkHeadersArray.forEach(link => {
+        const [urlPart, relPart] = link.split('; ');
+        const url = urlPart.slice(1, -1); // remove < and >
+        const rel = relPart.replace('rel="', '').replace('"', '');
+        linkHeadersMap[rel] = url;
+      });
+    
+      return linkHeadersMap;
+    }
+        // setUserListAll(users.data);
         setUserList(users.data);
+
+        const linkHeader = users.headers.link;
+        const linkParsed = parseLinkHeader(linkHeader);
+        console.log(linkParsed.next);
+        setNextPageUrl(linkParsed.next);
+
+
       } catch (error) {
         console.log(error);
       }
     }
-    getUserList();
-  },[]);
 
   function UserListTable(){
     return (
@@ -61,16 +84,24 @@ function App() {
         </table>
     )
   }
+
+  function Pagination(){
+    function handleNextClick(){
+      getUserList(nextPageUrl);
+    }
+    return(
+      <div>
+        <button>Previous</button>
+        <button onClick={handleNextClick}>Next</button>
+      </div>
+    )
+  }
   return (
     <div className="App">
       <main>
-        <SearchButton 
-        setUserList = {setUserList}
-        userListAll={userListAll}
-        setSearchInputValue={setSearchInputValue}
-        searchInputValue={searchInputValue}
-        />
+        {/* <SearchButton setUserList = {setUserList}userListAll={userListAll}setSearchInputValue={setSearchInputValue} searchInputValue={searchInputValue}/> */}
         <UserListTable/>
+        <Pagination/>
       </main>
     </div>
   );
